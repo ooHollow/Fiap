d5d2f9754296aed0a378f5ea9db74fd513baf0f3
Hollow <velpointl2@gmail.com>
2026-04-15 13:47:16 -0300

    Implementacao de API

---
+++ Projetos/API.py
@@ -0,0 +1,21 @@
+import requests
+
+base_url = "https://pokeapi.co/api/v2"
+
+def get_pokemon(name):
+    url = f'{base_url}/pokemon/{name}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f'Failed to get pokemon {response}')
+
+poke_name = "pikachu"
+pokemon_info = get_pokemon(poke_name)
+
+if pokemon_info:
+    print(f'Name:   {pokemon_info["name"].capitalize()}')
+    print(f'ID:     {pokemon_info["id"]}')
+    print(f'Height: {pokemon_info["height"]}')
+    print(f'Weight: {pokemon_info["weight"]}')
